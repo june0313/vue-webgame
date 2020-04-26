ec639b704d70c6c12782228e9c0ac1b45a1e1304
Young Jun Park <june0313@gmail.com>
2020-04-26 16:08:58 +0900

    Event Bus 기반 틱택토 구현

--- 7.틱택토EventBus2/TdComponent.vue
+++ 7.틱택토EventBus2/TdComponent.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+    import EventBus from './EventBus';
+
     export default {
         name: "TdComponent",
         props: {
@@ -12,58 +14,9 @@
         },
         methods: {
             onClickTd() {
-                if (this.cellData) {
-                    return;
-                }
-
-                const rootData = this.$root.$data;
-                this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
-
-                if (this.isWin(rootData)) {
-                    rootData.winner = rootData.turn;
-                    rootData.turn = 'O';
-                    rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
-                } else {
-                    let all = true;
-                    rootData.tableData.forEach(row => {
-                        row.forEach(cell => {
-                            if (!cell) {
-                                all = false;
-                            }
-                        });
-                    });
-
-                    if (all) {
-                        rootData.winner = '';
-                        rootData.turn = 'O';
-                        rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
-                    } else {
-                        rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
-                    }
-                }
+                if (this.cellData) return;
+                EventBus.$emit('clickTd', this.rowIndex, this.cellIndex);
             },
-            isWin(rootData) {
-                if (rootData.tableData[this.rowIndex][0] === rootData.turn &&
-                    rootData.tableData[this.rowIndex][1] === rootData.turn &&
-                    rootData.tableData[this.rowIndex][2] === rootData.turn) {
-                    return true;
-                }
-                if (rootData.tableData[0][this.cellIndex] === rootData.turn &&
-                    rootData.tableData[1][this.cellIndex] === rootData.turn &&
-                    rootData.tableData[2][this.cellIndex] === rootData.turn) {
-                    return true;
-                }
-                if (rootData.tableData[0][0] === rootData.turn &&
-                    rootData.tableData[1][1] === rootData.turn &&
-                    rootData.tableData[2][2] === rootData.turn) {
-                    return true;
-                }
-                if (rootData.tableData[0][2] === rootData.turn &&
-                    rootData.tableData[1][1] === rootData.turn &&
-                    rootData.tableData[2][0] === rootData.turn) {
-                    return true;
-                }
-            }
         }
     }
 </script>
--- 7.틱택토EventBus2/TicTacToe.vue
+++ 7.틱택토EventBus2/TicTacToe.vue
@@ -8,6 +8,7 @@
 
 <script>
     import TableComponent from "./TableComponent";
+    import EventBus from './EventBus';
 
     export default {
         components: {TableComponent},
@@ -22,7 +23,59 @@
                 winner: '',
             };
         },
-        methods: {},
+        methods: {
+            onClickTd(rowIndex, cellIndex) {
+                this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+
+                if (this.isWin(rowIndex, cellIndex)) {
+                    this.winner = this.turn;
+                    this.turn = 'O';
+                    this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+                } else {
+                    let all = true;
+                    this.tableData.forEach(row => {
+                        row.forEach(cell => {
+                            if (!cell) {
+                                all = false;
+                            }
+                        });
+                    });
+
+                    if (all) {
+                        this.winner = '';
+                        this.turn = 'O';
+                        this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+                    } else {
+                        this.turn = this.turn === 'O' ? 'X' : 'O';
+                    }
+                }
+            },
+            isWin(rowIndex, cellIndex) {
+                if (this.tableData[rowIndex][0] === this.turn &&
+                    this.tableData[rowIndex][1] === this.turn &&
+                    this.tableData[rowIndex][2] === this.turn) {
+                    return true;
+                }
+                if (this.tableData[0][cellIndex] === this.turn &&
+                    this.tableData[1][cellIndex] === this.turn &&
+                    this.tableData[2][cellIndex] === this.turn) {
+                    return true;
+                }
+                if (this.tableData[0][0] === this.turn &&
+                    this.tableData[1][1] === this.turn &&
+                    this.tableData[2][2] === this.turn) {
+                    return true;
+                }
+                if (this.tableData[0][2] === this.turn &&
+                    this.tableData[1][1] === this.turn &&
+                    this.tableData[2][0] === this.turn) {
+                    return true;
+                }
+            }
+        },
+        created() {
+            EventBus.$on('clickTd', this.onClickTd);
+        }
     };
 </script>
 
